2f8e80b9f1860411dd7a868c281b888c1e18dd42
Mac <mac@Macs-MacBook-Air.local>
2021-11-09 18:28:45 +0900

    hi

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -32,7 +32,5 @@ chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
 
-nohup java -jar \
-#    -Dspring.config.location=classpath:/application.properties,classpath:/home/ec2-user/app/application-oauth.properties \
-#    -Dspring.profiles.active=real \
+nohup java -jar
     $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
